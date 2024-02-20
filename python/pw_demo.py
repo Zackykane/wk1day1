f5d5668ebb22d3e7f760cccac67af18c411da54d
Zackykane <zackwilliams828@gmail.com>
2024-02-19 20:49:26 -0500

    first commit

---
+++ python/pw_demo.py
@@ -0,0 +1,13 @@
+def namer(f_name):
+    print(f"Welcome to CT {f_name}")
+    print("This will run too")
+# namer("Zack")
+# print(namer("Greyson") + namer("Bonnie"))
+print(namer("Zack"))
+# print(namer("Haley")) == 3
+# name2 = "Rocky"
+
+# def adder(num1, num2):
+#    return num1 + num2
+
+# print(adder(5, 6))
